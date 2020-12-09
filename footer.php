f4d060a4fbc9dea4f0dc4f2f2d28153cefc70f14
academy-glo <saphin14@rambler.ru>
2020-12-09 18:20:57 +0300

    create: footer & send.php

---
+++ footer.php
@@ -0,0 +1,152 @@
+<footer class="footer">
+    <div class="container">
+      <div class="footer-wrapper">
+        <img src="img/vartical-logo.svg" alt="Best tour plan" class="logo footer__logo">
+        <div class="footer__list footer__categories">
+          <h3 class="footer__title">ALL CATEGORIES
+          </h3>
+          <ul class="footer__ul">
+            <li class="footer__item"><a href="#" class="footer__link">All Deals
+              </a></li>
+            <li class="footer__item"><a href="#" class="footer__link">Hotels</a></li>
+            <li class="footer__item"><a href="#" class="footer__link">Activities</a></li>
+            <li class="footer__item"><a href="#" class="footer__link">Spa Packages
+              </a></li>
+            <li class="footer__item"><a href="#" class="footer__link">Hotel Day Packages
+              </a></li>
+            <li class="footer__item"><a href="#" class="footer__link">Restaurants
+              </a></li>
+            <li class="footer__item"><a href="#" class="footer__link">Fitness
+              </a></li>
+            <li class="footer__item"><a href="#" class="footer__link">Rodrigues</a></li>
+          </ul>
+        </div>
+        <!-- /.footer__list -->
+        <div class="footer__list footer__additional">
+          <h3 class="footer__title">ADDITIONAL INFORMATION
+          </h3>
+          <ul class="footer__ul">
+            <li class="footer__item"><a href="#" class="footer__link">About Us
+              </a></li>
+            <li class="footer__item"><a href="#" class="footer__link">Contact Us</a></li>
+            <li class="footer__item"><a href="#" class="footer__link">How does it work?
+              </a></li>
+            <li class="footer__item"><a href="#" class="footer__link">Frequently Asked Questions
+              </a></li>
+            <li class="footer__item"><a href="#" class="footer__link">Deals.mu loyalty program
+              </a></li>
+            <li class="footer__item"><a href="#" class="footer__link">Promote your Business on BTP
+              </a></li>
+          </ul>
+        </div>
+        <!-- /.footer__list -->
+        <div class="footer__social-network">
+          <h3 class="footer__headline footer__headline--inline">Social Network</h3>
+          <div class="footer__social-link">
+            <a href="#" class="footer__link"><img src="img/facebook.svg" alt="icon: facebook"></a>
+            <a href="#" class="footer__link"><img src="img/youtube.svg" alt="icon: youtube"></a>
+            <a href="#" class="footer__link"><img src="img/instagram.svg" alt="icon: instagram"></a>
+          </div>
+          <!-- /.footer__social-link -->
+        </div>
+        <!-- /.footer__social-network -->
+        <div class="footer__list footer__legal">
+          <h3 class="footer__title">LEGAL INFORMATION
+          </h3>
+          <ul class="footer__ul">
+            <li class="footer__item"><a href="#" class="footer__link">Terms & Conditions
+              </a></li>
+            <li class="footer__item"><a href="#" class="footer__link">Disclaimer
+              </a></li>
+            <li class="footer__item"><a href="#" class="footer__link">Cancellation policy
+              </a></li>
+            <li class="footer__item"><a href="#" class="footer__link">Privacy
+              </a></li>
+          </ul>
+        </div>
+        <!-- /.footer__list -->
+        <div class="footer__contact-details">
+          <h3 class="footer__title footer__title--mb-3">Contact Details</h3>
+          <p class="footer__text">
+            Feel free to contact us by phone, email or by our contact form
+          </p>
+          <ul class="footer__ul">
+            <li class="footer__item footer__item--mb-2">
+              <div class="footer__icon-wrapper"><img class="footer-icon" src="img/map-marker.svg" alt="icon: map" />
+              </div>
+              9748 Blossom Hill Rd undefined Lansing, Idaho 68545 United States
+            </li>
+            <li class="footer__item footer__item--mb-2">
+              <div class="footer__icon-wrapper"><img class="footer-icon" src="img/phone-tube.svg"
+                  alt="icon: phone-tube" /></div>
+              <div>
+                <p class="footer__phone">Tel (business hours): <a href="tel:2691500" class="footer__link">269 1500</a>
+                </p>
+                <p class="footer__phone">Tel (hotline) Monday - Saturday: <a href="tel:52566138"
+                    class="footer__link">52-56-61-38</a>
+                  (08:00&nbsp;am – 20:00&nbsp;pm)</p>
+                <p class="footer__phone">Tel (hotline) Sunday: <a href="tel:52566138"
+                    class="footer__link">52-56-61-38</a> (08:00&nbsp;am –
+                  14:00&nbsp;pm)</p>
+              </div>
+              <!-- /.Этот див нужен для оборачивания текста в ссылки чтобы и текст и иконка стояли на одном уровне и отталкивались -->
+            </li>
+            <li class="footer__item footer__item--mb-2">
+              <div class="footer__icon-wrapper"><img class="footer-icon" src="img/email.svg" alt="icon: email" /></div>
+              cherly.lawson@example.com
+            </li>
+          </ul>
+        </div>
+        <!-- /.footer__contact-details -->
+        <div class="footer__contact-form">
+          <h3 class="footer__title footer__title--mb-3">Send us a message</h3>
+          <form action="send.php" method="POST" class="footer__form">
+            <input type="text" class="input footer__input" placeholder="Your Full Name*" name="name">
+            <input type="text" class="input footer__input" placeholder="Phone Number*" name="phone">
+            <textarea class="message footer__message" placeholder="Message" name="message"></textarea>
+            <!-- /.последняя запись нужна чтобы сделать целый блок для сообшения  -->
+            <button class="button footer__button" type="submit">Send</button>
+            <span class="footer__info">* Required Fields</span>
+          </form>
+        </div>
+        <!-- /.footer__contact-form -->
+        <div class="footer-end">
+          <a href="#" class="footer__link footer-end__link">Disclaimer</a>
+          <a href="#" class="footer__link footer-end__link">Conditions of use </a>
+          <a href="#" class="footer__link footer-end__link">Cancellation policy</a>
+        </div>
+        <img src="img/end.svg" alt="end" class="footer__photo">
+        <!-- /.footer__end-->
+        <!-- /.Этот див нужен для оборачивания текста в ссылки чтобы и текст и иконка стояли на одном уровне и отталкивались -->
+      </div>
+      <!-- /.footer-wrapper -->
+    </div>
+    <!-- /.container -->
+  </footer>
+  <!-- /.footer -->
+  <div class="modal">
+    <div class="modal__overlay"></div>
+    <!-- /.modal__overlay -->
+    <div class="modal__dialog">
+      <a href="#" class="modal__close">
+        <img src="img/close.svg" alt="icon: close">
+      </a>
+      <h3 class="modal__title modal__title--mb-3">Send us a message</h3>
+      <form action="send.php" method="POST" class="modal__form">
+        <input type="text" class="input modal__input" placeholder="Your Full Name*" name="name">
+        <input type="text" class="input modal__input" placeholder="Phone Number*" name="phone">
+        <textarea class="message modal__message" placeholder="Message" name="message"></textarea>
+        <!-- /.последняя запись нужна чтобы сделать целый блок для сообшения  -->
+        <button class="button modal__button" type="submit">Send</button>
+        <span class="modal__info">* Required Fields</span>
+      </form>
+    </div>
+    <!-- /.modal__dialog -->
+  </div>
+  <!-- /.modal -->
+  <script src="js/jquery-3.5.1.min.js"></script>
+  <script src="js/swiper-bundle.min.js"></script>
+  <script src="js/main.js"></script>
+</body>
+
+</html>
--- js/main.js
+++ js/main.js
@@ -1,34 +1,47 @@
-var hotelSlider = new Swiper(".hotel-slider", {
-  // Optional parameters
-  loop: true,
-  // Navigation arrows
-  navigation: {
-    nextEl: ".hotel-slider__button--next",
-    prevEl: ".hotel-slider__button--prev",
-  },
-  keyboard: {
-    enabled: true,
-    onlyInViewport: false,
-  },
-  effect: "cube",
-  fadeEffect: {
-    crossFade: true,
-  },
-});
-var reviewsSlider = new Swiper(".reviews-slider", {
-  // Optional parameters
-  loop: true,
-  // Navigation arrows
-  navigation: {
-    nextEl: ".reviews-slider__button--next",
-    prevEl: ".reviews-slider__button--prev",
-  },
-});
+$(document).ready(function () {
+  var hotelSlider = new Swiper(".hotel-slider", {
+    // Optional parameters
+    loop: true,
+    // Navigation arrows
+    navigation: {
+      nextEl: ".hotel-slider__button--next",
+      prevEl: ".hotel-slider__button--prev",
+    },
+    keyboard: {
+      enabled: true,
+      onlyInViewport: false,
+    },
+    effect: "cube",
+    fadeEffect: {
+      crossFade: true,
+    },
+  });
+  var reviewsSlider = new Swiper(".reviews-slider", {
+    // Optional parameters
+    loop: true,
+    // Navigation arrows
+    navigation: {
+      nextEl: ".reviews-slider__button--next",
+      prevEl: ".reviews-slider__button--prev",
+    },
+  });
 
-var menuButton = document.querySelector(".menu-button");
-menuButton.addEventListener('click', function () {
-  console.log("Клик по кнопке меню");
-  document
-    .querySelector(".navbar-bottom")
-    .classList.toggle("navbar-bottom--visible");
-})
+  var menuButton = document.querySelector(".menu-button");
+  menuButton.addEventListener("click", function () {
+    console.log("Клик по кнопке меню");
+    document
+      .querySelector(".navbar-bottom")
+      .classList.toggle("navbar-bottom--visible");
+  });
+
+  var modalButton = $("[data-toggle=modal]");
+  modalButton.on("click", openModal);
+
+  function openModal() {
+    var modalOverlay = $(".modal__overlay");
+    var modalDialog = $(".modal__dialog");
+    modalOverlay.addСlas("modal__overlay--visible");
+    modalDialog.addСlas("modal__dialog--visible");
+  }
+
+});
