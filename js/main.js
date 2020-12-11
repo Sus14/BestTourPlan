f1259e07f4216f9bb72f37c6bb6ebe7009b3d83c
academy-glo <saphin14@rambler.ru>
2020-12-11 19:23:30 +0300

    fix: footer

--- js/main.js
+++ js/main.js
@@ -35,13 +35,23 @@ $(document).ready(function () {
   });
 
   var modalButton = $("[data-toggle=modal]");
+  var closeModalButton = $(".modal__close");
   modalButton.on("click", openModal);
+  closeModalButton.on("click", closeModal)
 
   function openModal() {
     var modalOverlay = $(".modal__overlay");
     var modalDialog = $(".modal__dialog");
-    modalOverlay.addСlas("modal__overlay--visible");
-    modalDialog.addСlas("modal__dialog--visible");
+    modalOverlay.addClass("modal__overlay--visible");
+    modalDialog.addClass("modal__dialog--visible");
+  }
+
+  function closeModal(event) {
+    event.preventDefault();
+    var modalOverlay = $(".modal__overlay");
+    var modalDialog = $(".modal__dialog");
+    modalOverlay.removeClass("modal__overlay--visible");
+    modalDialog.removeClass("modal__dialog--visible");
   }
 
 });
